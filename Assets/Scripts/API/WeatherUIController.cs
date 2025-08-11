d64c9697fb17fb0372c155b59798bf84f03f0bbe
TomalikaFerdous <tomalika.ferdous@gmail.com>
2025-08-11 15:59:10 +0200

    Update WeatherUIController.cs

--- Assets/Scripts/API/WeatherUIController.cs
+++ Assets/Scripts/API/WeatherUIController.cs
@@ -229,3 +229,57 @@ public class WeatherTest : MonoBehaviour
         }
     }
 }
+
+using System;
+using Newtonsoft.Json;
+
+[Serializable]
+public class WeatherData
+{
+    [JsonProperty("main")]
+    public MainWeatherInfo Main { get; set; }
+    
+    [JsonProperty("weather")]
+    public WeatherDescription[] Weather { get; set; }
+    
+    [JsonProperty("name")]
+    public string CityName { get; set; }
+    
+    [JsonProperty("cod")]
+    public int StatusCode { get; set; }
+    
+    // Already in Celsius when using &units=metric
+    public float TemperatureInCelsius => Main?.Temperature ?? 0f;
+    public string PrimaryDescription => Weather?.Length > 0 ? Weather[0].Description : "Unknown";
+    
+    public bool IsValid => StatusCode == 200 && Main != null && !string.IsNullOrEmpty(CityName);
+}
+
+[Serializable]
+public class MainWeatherInfo
+{
+    [JsonProperty("temp")]
+    public float Temperature { get; set; }
+    
+    [JsonProperty("feels_like")]
+    public float FeelsLike { get; set; }
+    
+    [JsonProperty("humidity")]
+    public int Humidity { get; set; }
+    
+    [JsonProperty("pressure")]
+    public int Pressure { get; set; }
+}
+
+[Serializable]
+public class WeatherDescription
+{
+    [JsonProperty("main")]
+    public string Main { get; set; }
+    
+    [JsonProperty("description")]
+    public string Description { get; set; }
+    
+    [JsonProperty("icon")]
+    public string Icon { get; set; }
+}
